16b765a85d1f1efd53b630d3492a4e20ff44ed17
Sam <shneydik.sam@gmail.com>
2024-08-03 20:42:02 +0300

    Fixed all CW

--- SQL_3/cw_main.py
+++ SQL_3/cw_main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 from SQL_3.database_handler import execute_query
 
 app = Flask(__name__)
@@ -8,9 +7,7 @@ app = Flask(__name__)
 @app.route('/customers')
 def get_all_customers():
     query = "SELECT * FROM customers"
-
     data = execute_query(query=query)
-
     return data
 
 
--- SQL_3/database_handler.py
+++ SQL_3/database_handler.py
@@ -2,7 +2,8 @@ import sqlite3
 
 
 def execute_query(query, args=()):
-    with sqlite3.connect('chinook.db') as connection:
+    db_path = 'C:/Users/shney/PycharmProjects/Hillel/lms_flask_2024_07/chinook.db'
+    with sqlite3.connect(db_path) as connection:
         cursor = connection.cursor()
         cursor.execute(query, args)
         connection.commit()
